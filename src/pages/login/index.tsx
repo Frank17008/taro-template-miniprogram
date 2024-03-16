e976472216742840c0c4c52eafb7c6828c0aca7d
donglei <donglei@qq.com>
2024-03-16 10:02:50 +0800

    fix：登录新增记住密码功能；

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,7 @@
 import Taro, { useShareAppMessage, useShareTimeline } from '@tarojs/taro';
 import { useState } from 'react';
-import { View, Text, Button } from '@tarojs/components';
-import { Input } from '@nutui/nutui-react-taro';
+import { View, Text } from '@tarojs/components';
+import { Input, Button, Checkbox } from '@nutui/nutui-react-taro';
 import { Marshalling, Eye } from '@nutui/icons-react-taro';
 import * as serivce from '@/service/login';
 import { LoginResData, IForm } from './interface';
@@ -12,6 +12,7 @@ const Login = () => {
     username: '',
     password: '',
   });
+  const [rememberPWD, setRememberPWD] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
   const [inputType, setInputType] = useState<string>('password');
   useShareAppMessage((res) => {
@@ -43,6 +44,12 @@ const Login = () => {
       .then((res: LoginResData) => {
         setLoading(false);
         if (res?.statusCode === 200) {
+          //判断是否记住密码
+          if (rememberPWD) {
+            rememberPwdHandler(formValue);
+          } else {
+            Taro.removeStorageSync('rememberpwd');
+          }
           saveStorage(res);
           Taro.switchTab({ url: '/pages/home/index' });
         } else {
@@ -55,6 +62,44 @@ const Login = () => {
       });
   };
 
+  const rememberPwdHandler = (values) => {
+    let baseUsername = stringToBase64(values.username);
+    let basepwd = stringToBase64(values.password);
+    let baseTenantId = stringToBase64(values.tenantId);
+    let str = `${baseUsername}&&${basepwd}&&${baseTenantId}`;
+    Taro.setStorageSync('rememberpwd', str);
+  };
+  //字符串转base64
+  const stringToBase64 = (str) => {
+    let buffer = new ArrayBuffer(str.length * 3);
+    let view = new Uint8Array(buffer);
+    let pos = 0;
+    for (let i = 0; i < str.length; i++) {
+      let codePoint = str.charCodeAt(i);
+      // 1字节字符（U+0000 到 U+007F）
+      if (codePoint < 0x80) {
+        view[pos++] = codePoint;
+      }
+      // 2字节字符（U+0080 到 U+07FF）
+      else if (codePoint < 0x800) {
+        view[pos++] = 0xc0 | (codePoint >> 6);
+        view[pos++] = 0x80 | (codePoint & 0x3f);
+      }
+      // 3字节字符（U+0800 到 U+FFFF）
+      else if (codePoint < 0x10000) {
+        view[pos++] = 0xe0 | (codePoint >> 12);
+        view[pos++] = 0x80 | ((codePoint >> 6) & 0x3f);
+        view[pos++] = 0x80 | (codePoint & 0x3f);
+      }
+      // 4字节字符（U+10000 到 U+10FFFF）
+      else {
+        // 注意：微信小程序可能不支持4字节的UTF-8字符，因为JavaScript字符串的charCodeAt方法只返回2字节的值
+        throw new Error('Unsupported Unicode character');
+      }
+    }
+    let getArrayBuffer = buffer.slice(0, pos);
+    return Taro.arrayBufferToBase64(getArrayBuffer) || '';
+  };
   const saveStorage = (data) => {
     const userInfo = {
       account: data.account,
@@ -113,7 +158,10 @@ const Login = () => {
             </View>
           )}
         </View>
-        <Button className='form-btn' block shape='square' type='primary' loading={loading} onClick={onSubmit}>
+        <View className='remember-pwd'>
+          <Checkbox label='记住密码' labelPosition='right' checked={rememberPWD} onChange={(value) => setRememberPWD(value)}></Checkbox>
+        </View>
+        <Button shape='square' loading={loading} onClick={onSubmit}>
           登录
         </Button>
       </View>
